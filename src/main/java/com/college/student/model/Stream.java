aaa57edea81b94866a9d15e3ce7f8edafd0b254b
Time <hsarda21@gmail.com>
2020-05-15 07:25:34 +0530

    adding POJOs. Closed #2.

---
+++ src/main/java/com/college/student/model/Stream.java
@@ -0,0 +1,14 @@
+package com.college.student.model;
+
+public class Stream
+{
+    private String streamName;
+
+    public String getStreamName() {
+        return streamName;
+    }
+
+    public void setStreamName(String streamName) {
+        this.streamName = streamName;
+    }
+}
---
+++ src/main/java/com/college/student/model/Teacher.java
@@ -0,0 +1,14 @@
+package com.college.student.model;
+
+public class Teacher extends Person
+{
+    private Stream stream;
+
+    public Stream getStream() {
+        return stream;
+    }
+
+    public void setStream(Stream stream) {
+        this.stream = stream;
+    }
+}
