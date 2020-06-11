0f279564a924f415b82262fd82e8bc4776b7c054
Time <hsarda21@gmail.com>
2020-06-11 10:58:12 +0530

    Added Student List and updated controller. closes #16

--- student-web/src/main/java/com/college/student/controllers/StudentController.java
+++ student-web/src/main/java/com/college/student/controllers/StudentController.java
@@ -1,15 +1,26 @@
 package com.college.student.controllers;
 
+import com.college.student.services.StudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/students")
 @Controller
 public class StudentController
 {
-    @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listStudents()
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService)
     {
+        this.studentService = studentService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listStudents(Model model)
+    {
+        model.addAttribute("students",studentService.findAll());
+
         return "students/index";
     }
 }
