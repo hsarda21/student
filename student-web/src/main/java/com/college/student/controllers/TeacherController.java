1b24e3b0dca62fbf423c4cb26bb68bd034d13127
Time <hsarda21@gmail.com>
2020-06-11 11:05:10 +0530

    Added Teacher List and updated controller. closes #17

--- student-web/src/main/java/com/college/student/controllers/TeacherController.java
+++ student-web/src/main/java/com/college/student/controllers/TeacherController.java
@@ -1,15 +1,25 @@
 package com.college.student.controllers;
 
+import com.college.student.services.TeacherService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/teachers")
 @Controller
 public class TeacherController
 {
-    @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listTeachers()
+    TeacherService teacherService;
+
+    public TeacherController(TeacherService teacherService)
     {
+        this.teacherService = teacherService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listTeachers(Model model)
+    {
+        model.addAttribute("teachers",teacherService.findAll());
         return "teachers/index";
     }
 }
