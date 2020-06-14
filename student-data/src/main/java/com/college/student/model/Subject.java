de5f570572500ffb98d0400c829a6eb88b24cdd1
Time <hsarda21@gmail.com>
2020-06-14 09:39:53 +0530

    Added subject entity and specialities for teacher. closes #30

---
+++ student-data/src/main/java/com/college/student/model/Subject.java
@@ -0,0 +1,23 @@
+package com.college.student.model;
+
+public class Subject
+{
+    String subjectName;
+    Stream stream;
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public Stream getStream() {
+        return stream;
+    }
+
+    public void setStream(Stream stream) {
+        this.stream = stream;
+    }
+}
--- student-data/src/main/java/com/college/student/model/Teacher.java
+++ student-data/src/main/java/com/college/student/model/Teacher.java
@@ -1,8 +1,11 @@
 package com.college.student.model;
 
+import java.util.Set;
+
 public class Teacher extends Person
 {
     private Stream stream;
+    private Set<Subject> specialities;
 
     public Stream getStream() {
         return stream;
@@ -10,5 +13,13 @@ public class Teacher extends Person
 
     public void setStream(Stream stream) {
         this.stream = stream;
+    }
+
+    public Set<Subject> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Subject> specialities) {
+        this.specialities = specialities;
     }
 }
