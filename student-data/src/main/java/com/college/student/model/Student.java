12e24e169b700409a7372fac7a01fe8f3693b1a5
Time <hsarda21@gmail.com>
2020-06-14 09:42:40 +0530

    Added contact info to student. cloes #31

--- student-data/src/main/java/com/college/student/model/Student.java
+++ student-data/src/main/java/com/college/student/model/Student.java
@@ -1,8 +1,14 @@
 package com.college.student.model;
 
+import java.util.Set;
+
 public class Student extends Person
 {
     private Stream stream;
+    private String address;
+    private String city;
+    private String phone;
+    private Set<Subject> subjects;
 
     public Stream getStream() {
         return stream;
@@ -10,5 +16,37 @@ public class Student extends Person
 
     public void setStream(Stream stream) {
         this.stream = stream;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Set<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(Set<Subject> subjects) {
+        this.subjects = subjects;
     }
 }
