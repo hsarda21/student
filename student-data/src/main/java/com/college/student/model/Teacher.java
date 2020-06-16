3cce0a0f015f021341e23429791cf6adda14a439
Time <hsarda21@gmail.com>
2020-06-16 12:01:14 +0530

    Converted Teachers to JPA entities. closes #37

--- student-data/src/main/java/com/college/student/model/Teacher.java
+++ student-data/src/main/java/com/college/student/model/Teacher.java
@@ -1,11 +1,20 @@
 package com.college.student.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "teachers")
 public class Teacher extends Person
 {
+    @Column(name = "stream")
     private Stream stream;
+
+    @Column(name = "specialities")
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "teacher_specialities", joinColumns = @JoinColumn(name = "teacher_id"),
+            inverseJoinColumns = @JoinColumn(name = "subject_id"))
     private Set<Subject> specialities = new HashSet<>();
 
     public Stream getStream() {
